b80508ac863120ee328d8a021c37f2b66580c304
Xevion <xevion@xevion.dev>
2026-02-12 18:17:23 -0600

    fix(frontend): use stacked grid layout for lightbox placeholder instead of background-image

--- frontend/src/lib/components/Lightbox.svelte
+++ frontend/src/lib/components/Lightbox.svelte
@@ -222,26 +222,28 @@ function handlePointerUp() {
 				>
 					<div
 						style="transform: scale({zoomLevel}) translate({panOffset.x}px, {panOffset.y}px); transition: transform {isDragging ? '0s' : '0.15s'} ease;"
+						class="grid max-h-[90vh] max-w-[90vw]"
+						style:grid-template="1fr / 1fr"
 					>
-					<div
-						class="relative"
-						style:background-image={placeholderUrl ? `url(${placeholderUrl})` : undefined}
-						style:background-size="contain"
-						style:background-position="center"
-						style:background-repeat="no-repeat"
-					>
+						{#if placeholderUrl && !imageLoaded}
+							<img
+								src={placeholderUrl}
+								alt=""
+								class="col-start-1 row-start-1 h-[90vh] w-[90vw] object-contain"
+								aria-hidden="true"
+							/>
+						{/if}
 						<img
 							bind:this={imgEl}
 							src={fullImageUrl}
 							alt="Capture fullscreen view"
-							class="max-h-[90vh] max-w-[90vw] object-contain transition-opacity duration-300"
+							class="col-start-1 row-start-1 max-h-[90vh] max-w-[90vw] object-contain transition-opacity duration-300"
 							class:opacity-0={!imageLoaded}
 							loading="eager"
 							decoding="async"
 							draggable="false"
 							onload={() => (imageLoaded = true)}
 						/>
-					</div>
 					</div>
 
 					<!-- Capture info overlay -->
